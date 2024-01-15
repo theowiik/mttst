1a205ff11f891fdee1d3dca33636b409b97164c8
theowiik <theo.le.wiik@gmail.com>
2024-01-15 13:40:00 +0100

    Update CMakeLists.txt and add Mathz class with greater_or_equal function

--- cpp-app/test.cpp
+++ cpp-app/test.cpp
@@ -1,9 +1,35 @@
 #define CATCH_CONFIG_MAIN
 
 #include "catch.hpp"
+#include "mathz.cpp"
 
-int theAnswer() { return 6 * 9; }
+int theAnswer() { return 40 + 2; }
 
 TEST_CASE("Life, the universe and everything", "[42][theAnswer]") {
     REQUIRE(theAnswer() == 42);
+}
+
+TEST_CASE("Greater or Equal - First Greater", "[greater_or_equal]") {
+    Mathz m;
+    REQUIRE(m.greater_or_equal(5, 3));
+}
+
+TEST_CASE("Greater or Equal - Equal Values", "[greater_or_equal]") {
+    Mathz m;
+    REQUIRE(m.greater_or_equal(4, 4));
+}
+
+TEST_CASE("Greater or Equal - First Lesser", "[greater_or_equal]") {
+    Mathz m;
+    REQUIRE_FALSE(m.greater_or_equal(2, 3));
+}
+
+TEST_CASE("Greater or Equal - Negative Values", "[greater_or_equal]") {
+    Mathz m;
+    REQUIRE(m.greater_or_equal(-1, -2));
+}
+
+TEST_CASE("Greater or Equal - Zero Values", "[greater_or_equal]") {
+    Mathz m;
+    REQUIRE(m.greater_or_equal(0, 0));
 }
