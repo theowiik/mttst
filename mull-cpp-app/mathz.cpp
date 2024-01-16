81834b095d28be291e1b54671c94f33ad5b0449d
theowiik <theo.le.wiik@gmail.com>
2024-01-16 15:00:02 +0100

    copy cpp-app for mull demo

---
+++ mull-cpp-app/mathz.cpp
@@ -0,0 +1,10 @@
+class Mathz {
+public:
+    bool less_or_equal(const double a, const double b) {
+        return a <= b;
+    }
+
+    bool greater_or_equal(const double a, const double b) {
+        return a >= b;
+    }
+};
